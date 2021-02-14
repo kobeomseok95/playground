860fdcc35245a222a6f6887d7bffb296f27ad9cc
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-02-14 20:26:58 +0900

    test

--- demo/src/main/java/com/example/demo/ex02_EventExample/Order.java
+++ demo/src/main/java/com/example/demo/ex02_EventExample/Order.java
@@ -3,7 +3,7 @@ package com.example.demo.ex02_EventExample;
 import lombok.*;
 
 @Getter
-@Builder
+@Builder(access = AccessLevel.PROTECTED)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order {
