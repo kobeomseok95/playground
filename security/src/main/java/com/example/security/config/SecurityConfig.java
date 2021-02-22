188923740d8af329fd9cbd750d1f43025fe08028
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-02-22 16:53:31 +0900

    Feat : 권한을 메서드 레벨에서 처리

--- security/src/main/java/com/example/security/config/SecurityConfig.java
+++ security/src/main/java/com/example/security/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity  // 스프링 시큐리티 필터가 스프링 필터체인에 등록된다.
+//securedEnabled > @Secured 활성화
+//prePostEnabled > @preAuthorize, @postAuthorize 활성화
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean   //해당 메서드의 리턴되는 오브젝트를 IoC로 등록
@@ -26,11 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
             .and()
                 .formLogin()
-                .loginPage("/loginForm")   //인증이 되지 않은 유저가 접근하는 경우 자동으로 로그인하게끔 하는 역할
+                .loginPage("/loginForm")        //인증이 되지 않은 유저가 접근하는 경우 자동으로 로그인하게끔 하는 역할
                 .loginProcessingUrl("/login")   //매개변수 주소가 호출되면 시큐리티가 가로채서 대신 로그인을 진행해준다.
                                                 // 이로 인해 컨트롤러에 /login을 작성하지 않아도 된다.
                 .defaultSuccessUrl("/");        // 로그인 성공시 이동 페이지 
                                                 // 즉, loginForm을 요청했을 때 인증되지 않았다면 로그인 후 '/' 경로로 이동, 
-                                                // 그렇지 않은 경우는 로그인후 원래 요청했떤 경로로 이동
+                                                // 그렇지 않은 경우는 로그인후 원래 요청했던 경로로 이동
     }
 }
--- security/src/main/java/com/example/security/controller/IndexController.java
+++ security/src/main/java/com/example/security/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.example.security.controller;
 import com.example.security.user.User;
 import com.example.security.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,7 +42,7 @@ public class IndexController {
     }
 
     //  SecurityConfig의 anyRequest.permitAll()에 의해
-//  시큐리티 필터가 낚지 않는다.
+    //  시큐리티 필터가 낚지 않는다.
     @GetMapping("/loginForm")
     public String loginForm() {
         return "loginForm";
@@ -57,5 +59,17 @@ public class IndexController {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         userRepository.save(user);  //단순히 save할 경우 시큐리티가 동작하지 않는다. 비밀번호를 암호화 하지 않았기 때문
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")  //특정 메서드의 권한을 부여하고 싶을 때 사용! 권한이 한가지 일 경우!
+    @GetMapping("/info")
+    public @ResponseBody String info() {
+        return "개인 정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')") //이 메서드가 실행되기 직전에 실행되는 어노테이션, 권한이 여러개 일 경우!
+    @GetMapping("/data")
+    public @ResponseBody String data() {
+        return "데이터 정보";
     }
 }
