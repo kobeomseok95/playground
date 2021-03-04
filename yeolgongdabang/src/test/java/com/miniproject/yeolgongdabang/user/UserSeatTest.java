91e819b59bb8639fa41964197358a6360ee84239
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-03-04 14:29:04 +0900

    Test : 이미 앉아있는 좌석일 경우 예외발생 test

--- yeolgongdabang/src/test/java/com/miniproject/yeolgongdabang/user/UserSeatTest.java
+++ yeolgongdabang/src/test/java/com/miniproject/yeolgongdabang/user/UserSeatTest.java
@@ -8,14 +8,20 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UserSeatTest {
 
-    User user;
+    User user1;
+    User user2;
     Seat seat;
 
     @BeforeEach
     void init() {
-        user = User.builder()
+        user1 = User.builder()
                 .id(1L)
                 .phone("12341234")
+                .build();
+
+        user2 = User.builder()
+                .id(2L)
+                .phone("56785678")
                 .build();
 
         seat = Seat.builder()
@@ -27,23 +33,48 @@ class UserSeatTest {
 
     @Test
     void 좌석_착석_테스트() throws Exception {
+
         //given
         UserSeat userSeat = UserSeat.builder()
                 .id(1L)
-                .user(user)
+                .user(user1)
                 .seat(seat)
                 .seated(true)
                 .build();
 
         //when
-        userSeat.takeASeat(user, seat);
+        userSeat.takeASeat(user1, seat);
 
         //then
         assertAll(
-                () -> assertEquals(user.getUserSeats().size(), 1),
-                () -> assertTrue(user.getUserSeats().contains(userSeat)),
-                () -> assertEquals(seat.getUser(), user),
+                () -> assertEquals(user1.getUserSeats().size(), 1),
+                () -> assertTrue(user1.getUserSeats().contains(userSeat)),
+                () -> assertEquals(seat.getUser(), user1),
                 () -> assertTrue(userSeat.isSeated())
         );
+    }
+
+    @Test
+    void 좌석_착석_테스트_이미_앉아있는_좌석일_경우() throws Exception {
+
+        // given
+        UserSeat userSeat = UserSeat.builder()
+                .id(1L)
+                .user(user1)
+                .seat(seat)
+                .seated(true)
+                .build();
+        userSeat.takeASeat(user1, seat);
+
+        // when
+        UserSeat userSeat2 = UserSeat.builder()
+                .id(2L)
+                .user(user2)
+                .seat(seat)
+                .seated(true)
+                .build();
+
+        // then
+        assertThrows(IllegalStateException.class, () -> userSeat.takeASeat(user2, seat));
     }
 }
