2427cd6b06137707a3c5a585a5cfe25c02c2dd3e
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-03-17 13:39:15 +0900

    Feat : jsp, servlet mvc 패턴 적용

---
+++ servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.servletmvc;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
+public class MvcMemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        List<Member> members = memberRepository.findAll();
+        req.setAttribute("members", members);
+        String viewPath = "/WEB-INF/views/members.jsp";
+        RequestDispatcher dispatcher = req.getRequestDispatcher(viewPath);
+        dispatcher.forward(req, resp);
+    }
+}
