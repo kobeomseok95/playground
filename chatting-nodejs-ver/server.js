865a06dc334f411bbce6d688fa4809829120aa9f
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-04-20 23:42:33 +0900

    채팅 추가기능 구현

--- chatting-nodejs-ver/server.js
+++ chatting-nodejs-ver/server.js
@@ -1,44 +1,43 @@
+// server.js
+
 var express = require('express');
 var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);    
 var path = require('path');
 
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    // 루트 페이지로 접속시 chat.pug 렌더링
-    res.render('chat');
+app.get('/', (req, res) => {  
+  res.render('chat');
 });
 
-var count = 1;
-// 채팅방 접속
-io.on('connection', function(socket){
-    console.log('user connected: ' + socket.id);
-    var name = count++ + '번째 손님';
-    socket.name = name;
-    io.to(socket.id).emit('create name', name);
-    io.emit('receive message', name + '님이 입장하셨습니다.');
+var count=1;
+io.on('connection', function(socket){   // 채팅방에 접속했을 때 - 1
+  	console.log('user connected: ', socket.id);  
+  	var name = "익명" + count++;                 
+	socket.name = name;
+  	io.to(socket.id).emit('create name', name);   
+	io.emit('new_connect', name);
+	
+	socket.on('disconnect', function(){   // 채팅방 접속이 끊어졌을 때 - 2
+	  console.log('user disconnected: '+ socket.id + ' ' + socket.name);
+	  io.emit('new_disconnect', socket.name);
+	});
 
-    // 채팅방 접속이 끊어졌을 때
-    socket.on('disconnect', function() {
-        console.log('user disconnected" ' + socket.id + ' ' + socket.name);
-        io.emit('receive message', socket.name + '님이 퇴장하셨습니다.');
-        count--;
-    });
-
-    // 메세지 전송
-    socket.on('send message', function(name, text){
-        var msg = name + ' : ' + text;
-        socket.name = name;
-        console.log(msg);
-        io.emit('receive message', msg);
-    });
-
+	socket.on('send message', function(name, text){   // 메세지를 보냈을 때 - 3
+		var msg = name + ' : ' + text;
+		if(name != socket.name)   // 닉네임을 바꿨을 때 
+			io.emit('change name', socket.name, name);
+		socket.name = name;
+    	console.log(msg);
+    	io.emit('receive message', msg);
+	});
+	
 });
 
-http.listen(3001, function() {
-    console.log('server on!');
-})
+http.listen(3000, function(){ 
+	console.log('server on..');
+});
