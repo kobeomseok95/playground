15f823f90e091b6caf251420c137d6c30890fc10
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-05-09 23:39:59 +0900

    로깅 필터 적용

---
+++ msa/apigateway/api-gateway/src/main/java/com/example/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,68 @@
+package com.example.apigateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(LoggingFilter.Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+//        return ((exchange, chain) -> {
+//            ServerHttpRequest request = exchange.getRequest();
+//            ServerHttpResponse response = exchange.getResponse();
+//
+//            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
+//            if (config.isPreLogger()) {
+//                log.info("Logging Filter Start: request id -> {}", request.getId());
+//            }
+//
+//            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+//                if (config.isPostLogger()) {
+//                    log.info("Logging Filter End: response code -> {}", response.getStatusCode());
+//                }
+//            }));
+//        });
+
+        /** 직접 GatewayFilter 정의
+         *      WebFluxo에서는 ServerWebExchange로 request, response를 받을 수 있다.
+         */
+        GatewayFilter filter = new OrderedGatewayFilter((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            log.info("Logging Filter baseMessage: {}", config.getBaseMessage());
+            if (config.isPreLogger()) {
+                log.info("Logging PRE Filter Start: request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Logging POST Filter End: response code -> {}", response.getStatusCode());
+                }
+            }));
+        }, Ordered.LOWEST_PRECEDENCE);
+
+        return filter;
+    }
+
+    @Data
+    public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
