3ecaa398aca5e3d5b65211ccc285893752ce4db6
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-05-28 16:20:02 +0900

    jpa 복습 프로젝트 생성

---
+++ jpa-training/cascade-join/src/main/java/com/jpa/cascadejoinexample/CascadeJoinExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.cascadejoinexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CascadeJoinExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CascadeJoinExampleApplication.class, args);
+    }
+
+}
---
+++ jpa-training/cascade-join/src/test/java/com/jpa/cascadejoinexample/CascadeJoinExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.cascadejoinexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CascadeJoinExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
