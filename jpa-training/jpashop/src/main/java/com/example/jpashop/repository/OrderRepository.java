000027379ca55b355bdb941f33085c7bc020168a
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2021-06-04 15:23:22 +0900

    order 쿼리 수정

--- jpa-training/jpashop/src/main/java/com/example/jpashop/repository/OrderRepository.java
+++ jpa-training/jpashop/src/main/java/com/example/jpashop/repository/OrderRepository.java
@@ -3,12 +3,13 @@ package com.example.jpashop.repository;
 import com.example.jpashop.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @Query("select distinct o from Order o join fetch o.orderItems oi join fetch oi.item i" +
-            " join fetch o.delivery d")
-    Optional<Order> findIdFetch(Long orderId);
+            " join fetch o.delivery d where o.id = :orderId")
+    Optional<Order> findIdFetch(@Param("orderId") Long orderId);
 }
