7b290ab0b1cc355f8c0a6758de49a13cbbab2e83
kobeomseok95 <kobumssh@naver.com>
2021-06-30 02:37:53 +0900

    test

--- single_play_number_guessing_with_tdd/src/main/java/numberGuess/models/AppModel.java
+++ single_play_number_guessing_with_tdd/src/main/java/numberGuess/models/AppModel.java
@@ -34,7 +34,7 @@ public class AppModel {
         return output;
     }
 
-    // TODO : 리팩터링 완료2
+    // TODO : 리팩터링 완료3 
     public void processInput(String input) {
         if (!isSinglePlayMode) {
             selectGameMode(input);
