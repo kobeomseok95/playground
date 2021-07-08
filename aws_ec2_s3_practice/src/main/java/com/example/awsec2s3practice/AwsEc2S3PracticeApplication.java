54a979f4cf1eb0960ff47bee416a23fbb8d14e89
kobeomseok95 <kobumssh@naver.com>
2021-07-08 13:04:16 +0900

    initial commit for aws connect

---
+++ aws_ec2_s3_practice/src/main/java/com/example/awsec2s3practice/AwsEc2S3PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsec2s3practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsEc2S3PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AwsEc2S3PracticeApplication.class, args);
+    }
+
+}
---
+++ aws_ec2_s3_practice/src/test/java/com/example/awsec2s3practice/AwsEc2S3PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsec2s3practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEc2S3PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
