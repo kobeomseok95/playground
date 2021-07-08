69d9c7135a28980916290f90c08f644e663b0379
kobeomseok95 <kobumssh@naver.com>
2021-07-08 14:51:40 +0900

    feat : 구조 변경, 인터페이스 생성

--- aws_ec2_s3_practice/src/main/java/com/example/awsec2s3practice/controllers/BoardController.java
+++ aws_ec2_s3_practice/src/main/java/com/example/awsec2s3practice/controllers/BoardController.java
@@ -1,10 +1,15 @@
-package com.example.awsec2s3practice;
+package com.example.awsec2s3practice.controllers;
 
+import com.example.awsec2s3practice.services.interfaces.BoardService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/boards")
+@RequiredArgsConstructor
 public class BoardController {
+
+    private final BoardService boardService;
 
     @GetMapping("/{boardID}")
     public void getBoard(@PathVariable("boardID") String boardId) {
---
+++ aws_ec2_s3_practice/src/main/java/com/example/awsec2s3practice/services/interfaces/BoardService.java
@@ -0,0 +1,4 @@
+package com.example.awsec2s3practice.services.interfaces;
+
+public interface BoardService {
+}
