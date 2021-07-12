196e84b9a1634e252435fa1a22323d3847454c5a
kobeomseok95 <kobumssh@naver.com>
2021-07-12 20:19:46 +0900

    init exception-guide

---
+++ exception-guide/src/main/java/com/example/exceptionguide/ExceptionGuideApplication.java
@@ -0,0 +1,13 @@
+package com.example.exceptionguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionGuideApplication.class, args);
+    }
+
+}
---
+++ exception-guide/src/test/java/com/example/exceptionguide/ExceptionGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exceptionguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
