3b83beb6155d4a829af068c52cee945682dda758
kobumssh <kobumssh@naver.com>
2021-09-08 23:15:51 +0900

    initial commit

---
+++ pattern-training/interface/src/test/java/com/example/manyimplements/ManyimplementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manyimplements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManyimplementsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
