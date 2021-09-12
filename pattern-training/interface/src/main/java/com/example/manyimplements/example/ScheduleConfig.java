a6555f560e72d4ad2db2c71a722ba17f456fd83b
kobumssh <kobumssh@naver.com>
2021-09-12 12:10:16 +0900

    스케줄링

---
+++ pattern-training/interface/src/main/java/com/example/manyimplements/example/ScheduleConfig.java
@@ -0,0 +1,19 @@
+package com.example.manyimplements.example;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@EnableScheduling
+@Configuration
+public class ScheduleConfig {
+
+    @Scheduled(cron = "0 0/1 * * * *")
+    public void test() {
+        System.out.println(LocalTime.now());
+        System.out.println("스케줄링 체크하기" + LocalDateTime.now());
+    }
+}
