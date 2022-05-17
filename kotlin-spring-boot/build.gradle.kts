958e9ab9ad35e10037315ac653bbaac8e9df5d10
kobeomseok95 <kobumssh@naver.com>
2022-05-17 20:14:27 +0900

    lombok 의존성 제거

--- kotlin-spring-boot/build.gradle.kts
+++ kotlin-spring-boot/build.gradle.kts
@@ -28,9 +28,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    compileOnly("org.projectlombok:lombok")
     runtimeOnly("com.h2database:h2")
-    annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
