013c44434521801ccff2a0e07a49847f88bbe2ad
kobeomseok95 <kobumssh@naver.com>
2022-05-17 20:12:15 +0900

    코틀린 프로젝트 셋팅

---
+++ kotlin-spring-boot/src/test/kotlin/com/hello/kotlin/KotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hello.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
