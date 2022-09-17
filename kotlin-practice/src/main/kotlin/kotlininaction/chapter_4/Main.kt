e1adf28a79ddf70f57741896487c4c5ad805f91d
kobeomseok95 <kobumssh@naver.com>
2022-09-17 19:33:17 +0900

    코틀린 인 액션 함수파트

--- kotlin-practice/src/main/kotlin/kotlininaction/chapter_4/Main.kt
+++ kotlin-practice/src/main/kotlin/kotlininaction/chapter_4/Main.kt
@@ -1,11 +1,38 @@
 package kotlininaction.chapter_4
 
-import kotlininaction.chapter_4.init.User
-
 fun main() {
-    println(User.newFacebookUser(1).nickname)
-    println(User.newSubscribingUser("kobumssh@naver.com").nickname)
-    println(User.googleUser("ga@gmail.com").nickname)
+    val log = listOf(
+        SiteVisit("/", 34.0, OS.ANDROID),
+        SiteVisit("/", 21.0, OS.WINDOWS),
+        SiteVisit("/products", 12.5, OS.LINUX),
+        SiteVisit("/products", 24.65, OS.WINDOWS),
+        SiteVisit("/", 63.123, OS.WINDOWS),
+    )
 }
 
-fun User.UserFactory.googleUser(email: String): User = User.newSubscribingUser("google" + email)
+fun byPath(path: String) = { siteVisit: SiteVisit ->
+    siteVisit.path === path
+}
+
+fun byOS(os: OS) = { siteVisit: SiteVisit ->
+    siteVisit.os === os
+}
+
+fun List<SiteVisit>.averageDurationFor(predicate: (SiteVisit) -> Boolean) = filter(predicate)
+    .map(SiteVisit::duration)
+    .average()
+
+fun List<SiteVisit>.averageDuration(os: OS) = filter { it.os === os }
+    .map{ it.duration }
+    .average()
+
+enum class OS {
+    WINDOWS, LINUX, MAC, IOS, ANDROID
+}
+
+data class SiteVisit(
+    val path: String,
+    val duration: Double,
+    val os: OS,
+)
+
