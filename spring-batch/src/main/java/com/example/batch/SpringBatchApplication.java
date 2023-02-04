46c87afdf6ba6e814a982eff13e1f3f39be8a3c1
kobeomseok95 <kobumssh@naver.com>
2023-02-05 01:46:53 +0900

    배치 스케줄링 실행

--- spring-batch/src/main/java/com/example/batch/SpringBatchApplication.java
+++ spring-batch/src/main/java/com/example/batch/SpringBatchApplication.java
@@ -3,8 +3,10 @@ package com.example.batch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableBatchProcessing
+@EnableScheduling
 @SpringBootApplication
 public class SpringBatchApplication {
 
