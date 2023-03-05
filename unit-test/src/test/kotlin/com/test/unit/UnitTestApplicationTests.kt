5cccb337f3e8e2f0b9b26b5405efb90775114aea
kobeomseok95 <kobumssh@naver.com>
2023-03-05 15:06:15 +0900

    init unit-test

---
+++ unit-test/src/test/kotlin/com/test/unit/UnitTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.test.unit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnitTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
