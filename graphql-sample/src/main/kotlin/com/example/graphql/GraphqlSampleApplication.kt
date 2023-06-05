5d0883dc9e2f9f3a8307037d13032dfc4887136b
kobeomseok95 <kobumssh@naver.com>
2023-06-05 22:25:00 +0900

    graphql init

---
+++ graphql-sample/src/main/kotlin/com/example/graphql/GraphqlSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.graphql
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GraphqlSampleApplication
+
+fun main(args: Array<String>) {
+    runApplication<GraphqlSampleApplication>(*args)
+}
