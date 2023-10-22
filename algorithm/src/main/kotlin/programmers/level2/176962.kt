8df4c63a2fc0b7fa3a86fbd5e3f4c165742034b4
kobeomseok95 <kobumssh@naver.com>
2023-10-22 18:28:57 +0900

    프로그래머스 과제 진행하기 풀이

--- algorithm/src/main/kotlin/programmers/level2/176962.kt
+++ algorithm/src/main/kotlin/programmers/level2/176962.kt
@@ -1,8 +1,53 @@
 package programmers.level2
 
+import java.util.*
+
 class `176962` {
+    private lateinit var todo: List<Plan>
+    private val paused = Stack<Plan>()
+    private val answer = mutableListOf<String>()
+
     fun solution(plans: Array<Array<String>>): Array<String> {
-        val sortedPlans = plans.map { plan ->
+        setTodoPlans(plans)
+
+        todo.dropLast(1).forEachIndexed { i, plan ->
+            val nextStartTime = todo[i + 1].startTime
+
+            if (plan.endTime <= nextStartTime) {
+                answer.add(plan.subject)
+                var remainTime = nextStartTime - plan.endTime
+
+                while (remainTime > 0 && paused.isNotEmpty()) {
+                    if (paused.peek().duration <= remainTime) {
+                        remainTime -= paused.peek().duration
+                        answer.add(paused.pop().subject)
+                    } else {
+                        val pop = paused.pop()
+                        paused.push(
+                            pop.copy(duration = pop.duration - remainTime)
+                        )
+                        remainTime = 0
+                    }
+                }
+            } else {
+                paused.push(
+                    plan.copy(
+                        duration = plan.endTime - nextStartTime
+                    )
+                )
+            }
+        }
+        answer.add(todo.last().subject)
+
+        while (paused.isNotEmpty()) {
+            answer.add(paused.pop().subject)
+        }
+
+        return answer.toTypedArray()
+    }
+
+    private fun setTodoPlans(plans: Array<Array<String>>) {
+        todo = plans.map { plan ->
             val (startHour, startMinute) = plan[1].split(":")
             val startIntTime = startHour.toInt() * 60 + startMinute.toInt()
             Plan(
@@ -10,103 +55,15 @@ class `176962` {
                 startTime = startIntTime,
                 duration = plan[2].toInt(),
             )
-        }.sortedBy{ it.startTime }
-
-        sortedPlans.indices.forEach { i ->
-            (i + 1 until sortedPlans.size).forEach { j ->
-                if (sortedPlans[i].endTime > sortedPlans[j].startTime) {
-                    sortedPlans[i].duration += sortedPlans[j].duration
-                } else {
-                    return@forEach
-                }
-            }
-        }
-
-        return sortedPlans.sortedBy { it.endTime }.map { it.subject }.toTypedArray()
+        }.sortedBy { it.startTime }
     }
 
     data class Plan(
         val subject: String,
-        var startTime: Int,
-        var duration: Int,
+        val startTime: Int,
+        val duration: Int,
     ) {
         val endTime: Int
             get() = startTime + duration
     }
 }
-
-fun main() {
-    `176962`().solution(
-        plans = arrayOf(
-            arrayOf("korean", "11:40", "30"),
-            arrayOf("english", "12:10", "20"),
-            arrayOf("math", "12:30", "40"),
-        )
-    ).onEach(::println) // korean, english, math
-    println()
-    `176962`().solution(
-        plans = arrayOf(
-            arrayOf("science", "12:40", "50"),
-            arrayOf("music", "12:20", "40"),
-            arrayOf("history", "14:00", "30"),
-            arrayOf("computer", "12:30", "100"),
-        )
-    ).onEach(::println)
-    println()
-    `176962`().solution(
-        plans = arrayOf(
-            arrayOf("aaa", "12:00", "20"),
-            arrayOf("bbb", "12:10", "30"),
-            arrayOf("ccc", "12:40", "10"),
-        )
-    ).onEach(::println)
-    println()
-    `176962`().solution(
-        plans = arrayOf(
-            arrayOf("1", "00:00", "30"),
-            arrayOf("2", "00:10", "40"),
-            arrayOf("3", "00:20", "10"),
-            arrayOf("4", "00:25", "10"),
-            arrayOf("5", "01:10", "10"),
-        )
-    ).onEach(::println)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
